2c6aa8c19bf6d4f75d463eca68a73b4b85c0954e
Romain G <rom1guyot@gmail.com>
2019-01-20 01:08:49 -0500

    feat: drop-down menus & construction page

--- app.js
+++ app.js
@@ -35,7 +35,12 @@ app.use(function(err, req, res, next) {
 
     // render the error page
     res.status(err.status || 500);
-    res.render('error');
+    if (err.status === 404) {
+        res.render('construction');
+    }
+    else {
+        res.render('error');
+    }
 });
 
 module.exports = app;
