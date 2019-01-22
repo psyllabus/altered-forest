26247f297114f3d3fe184f77276eb1877e7d3a2a
Romain G <rom1guyot@gmail.com>
2019-01-21 22:13:42 -0500

    feat: mobile menu

---
+++ public/javascripts/custom/tickets.js
@@ -0,0 +1,3 @@
+$(function () {
+    xProductBrowser("categoriesPerRow=3", "views=grid(20,3) list(60) table(60)", "categoryView=grid", "searchView=list", "id=my-store-16170922");
+});
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
     res.render('home', { title: 'Altered Forest' });
 });
+// router.get('/tickets', function (req, res, next) {
+//     res.render('tickets', { title: 'Altered Forest Tickets' });
+// });
 
 module.exports = router;
